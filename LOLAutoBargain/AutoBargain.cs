946cc2377257da4eb3c87c2d28fde11020965ba1
haipro287 <haipro287@gmail.com>
2021-08-02 14:30:04 +0700

    fix user-agent

--- LOLAutoBargain/AutoBargain.cs
+++ LOLAutoBargain/AutoBargain.cs
@@ -107,6 +107,12 @@ namespace LOLAutoBargain
 
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("token", landingToken);
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) LeagueOfLegendsClient/11.15.388.2387 (CEF 74) Safari/537.36");
+
+            var configResponse = client.GetAsync("https://bargain.lol.garena.vn/api/config");
+            var cfgmsg = await configResponse;
+            Console.WriteLine(await cfgmsg.Content.ReadAsStringAsync());
+            Console.WriteLine("---------------------------------------------------------");
 
             var profileResponse = client.GetAsync("https://bargain.lol.garena.vn/api/profile");
             var msg = await profileResponse;
